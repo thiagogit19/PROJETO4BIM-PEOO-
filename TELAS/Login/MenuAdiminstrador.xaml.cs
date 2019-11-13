95ba3796c87e1b796af73c03de643ec63c4f96a6
†ђเลφ๏ җล√เэя <thiago_xavier.2002@hotmail.com>
2019-11-13 17:13:11 -0300

    TELAS PARCIAMENTE FINALIZADAS. JOGADOR CONCLUÍDO

---
+++ TELAS/Login/MenuAdiminstrador.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Login
+{
+    /// <summary>
+    /// Lógica interna para MenuAdiminstrador.xaml
+    /// </summary>
+    public partial class MenuAdiminstrador : Window
+    {
+        public MenuAdiminstrador()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
